f84a34b8c3cb41f68f7b218e7d0e84340c3e5b51
Antonio Troina <antoniotroina@antonios-mbp.home>
2015-12-05 23:49:00 +0000

    Add contact table

--- init.sql
+++ init.sql
@@ -3,9 +3,21 @@ USE DBWEB;
 CREATE TABLE `page`
 (
   id VARCHAR(100) NOT NULL PRIMARY KEY,
+  title VARCHAR(128),
   summary VARCHAR(500),
   content TEXT
 );
 
-INSERT INTO `page` (id, summary, content)
-    VALUES ('index', 'This is the index', 'This is the content');
+CREATE TABLE `contact`
+(
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  type VARCHAR(10) NOT NULL,
+  content VARCHAR(100) NOT NULL
+);
+
+INSERT INTO `page` (id, title, summary, content)
+    VALUES ('index', 'This is the title', 'This is the summary', 'This is the content');
+
+INSERT INTO `contact` (id, type, content)
+    VALUES (1, 'EMAIL', 'antonio.troina@me.com'),
+           (2, 'MOBILE', '+44(0)7428434808');
